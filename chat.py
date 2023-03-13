c351ef557bcb2a68bc166c62da5ff103c97d2d99
orikahmadov <orkhan.ahmadov28@.gmail.com>
2023-03-13 14:45:25 +0300

    initial commit

---
+++ chat.py
@@ -0,0 +1,53 @@
+import os, colorama
+import openai
+from datetime import datetime
+from rich.console import Console,Style
+from rich.prompt import Prompt
+
+API_KEY =  os.environ["OPENAI_API_KEY"]
+openai.api_key = API_KEY
+console =  Console()
+current_year = datetime.now().strftime("%Y")
+user_style =  Style(color="cyan",bold=True)
+bot_style =  Style(color="red",bold=False)
+waiting_style =  Style(color="yellow",bold=True)
+conversation =   [{"role": "system", "content": """You are  my smart bot assistant. Current year is {}. 
+""".format(current_year)}]
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def main():
+    while True:
+        try:
+            user_input = Prompt.ask("User: ")
+            conversation.append({"role": "user", "content": user_input})
+            console.log(f"Bot is thinking...", style=waiting_style)
+            response = openai.ChatCompletion.create(
+                model = "gpt-3.5-turbo",
+                messages=conversation
+            )
+            seperator  =  "*"*100
+            console.log(seperator, style="bold magenta")
+            reply_bot = response["choices"][0]["message"]["content"]
+            conversation.append({"role": "system", "content": reply_bot})
+            console.log(f"Bot: {reply_bot}", style="bold green")
+        except KeyboardInterrupt:
+            save_conversationn = input("Do you want to save the conversation? (y/n): ")
+            if save_conversationn.lower() == "y":
+                save_conversation()
+                print("Thanks for chatting with me!")
+                break
+            else:
+                print("Thanks for chatting with me!")
+                break
+
+def save_conversation():
+    with open("conversation.txt", "a") as f: # a for append
+        f.write(timestamp + "\n")
+        for message in conversation:
+            f.write(message["role"] + ": " + message["content"] + "\n")
+        f.write("\n")
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ image.py
@@ -0,0 +1,73 @@
+import openai
+import requests
+import os
+
+""" This is a simple chatbot that uses the OpenAI API to generate an image"""
+
+API_KEY = os.environ["OPENAI_API_KEY"]
+
+url =  "https://api.openai.com/v1/images/variations"
+def create_an_image():
+    while True:
+        try:
+            print("Welcome to the image creator!")
+            user_input = input("Describe an image: ")
+            n_times =  int(input("How many images do you want to create? "))
+            number_of_images = 0
+            if n_times > 1:
+                number_of_images = n_times
+            elif n_times > 10:
+                print("Sorry, you can only create 10 images at a time.")
+                number_of_images = 10
+            else:
+                number_of_images = 1
+            response =  openai.Image.create(
+                prompt=user_input,
+                n=number_of_images,
+                size = "1024x1024"
+            )
+            if response:
+                #download the image and save it
+                for i in range(number_of_images):
+                    image_url = response["data"][i]["url"]
+                    image_data = requests.get(image_url).content
+                    name_of_image =  hash(user_input) + i
+                    if not os.path.exists("images"):
+                        os.mkdir("images")
+                    with open(f"images/{name_of_image}.jpg", "wb") as handler:
+                        handler.write(image_data)
+                print(f"Image created {os.path.abspath('images')}")
+                clean_up = input("Do you want to delete the images? (y/n): ")
+                if clean_up.lower() == "y":
+                    #delete all the images in the images folder
+                    for file in os.listdir("images"):
+                        os.remove(os.path.join("images", file))
+                    print("Images deleted")
+                    another_image = input("Do you want to create another image? (y/n): ")
+                    if another_image.lower() == "y":
+                        continue
+                    else:
+                        print("Bye!")
+                        break
+            else:
+                print("Sorry, I couldn't create an image for you.")
+        except KeyboardInterrupt:
+            print("Bye!")
+            break
+        except Exception as e:
+            print("Sorry, I couldn't create an image for you.")
+            print(e)
+
+
+
+
+if __name__ == "__main__":
+    create_an_image()
+
+
+
+
+
+    
+
+
