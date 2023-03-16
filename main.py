b90f3b44796d95dd9f491b413690fc42fd763c8c
orikahmadov <orkhan.ahmadov28@.gmail.com>
2023-03-16 13:30:26 +0300

    added changes to the main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import chat
 import image,essay
+import programmer_assistant
 from rich.console import Console,Style
-from pyarmor.pyarmor import main_entry
-from pyarmor import __version__ 
 
 console = Console()
 
 while True:
-    console.print("Welcome to the Openai chat!",style="bold yellow")
+    console.print("Welcome to the Openai chat! (General Chat)",style="bold yellow")
     console.print("1. Chat (Chat with the bot about anything)",style="bold green")
-    console.print("2. Create an image",style="bold cyan")
-    console.log("3. Create Essay",style="bold yellow")
-    console.print("4. Exit",style="bold red")
+    console.print("2. Create an image (E.g Draw a cat in Dali style)",style="bold cyan")
+    console.print("3. Summarize and Simplify text from PDF (Pass pdf as input)",style="bold yellow")
+    console.print("4. Create a program (Writes pseudecode and program)",style="bold magenta")
+    console.print("5. Exit",style="bold red")
     print("\n")
     choice = int(input("Enter your choice: "))
     if choice == 1:
@@ -21,8 +21,10 @@ while True:
     elif choice == 3:
         essay.main()
     elif choice == 4:
-        print("Thank you for using our program!")
+        programmer_assistant.main()
+    elif choice == 5:
         break
     else:
-        print("Invalid choice")
+        console.print("Invalid choice!",style="bold red")
+
 
